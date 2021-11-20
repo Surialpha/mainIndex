0f36ec054d5f6dddc89f626da9284539ec7abb18
Surialpha <sgp_96@live.com>
2021-11-19 19:26:05 -0500

    Adding nav and clock

---
+++ vendor/js/functions.js
@@ -0,0 +1,16 @@
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  document.getElementById('clock').innerHTML =  h + ":" + m ;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+startTime();
